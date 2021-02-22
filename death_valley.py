f2be1c08abd49151063bd19274ec35ddb3cdec9f
Abby Brown <abby_brown3@baylor.edu>
2021-02-22 15:38:15 -0600

    Almost done for the day!

--- death_valley.py
+++ death_valley.py
@@ -1,49 +1,68 @@
-#!) Handle error checking using try and except
-# 2) change file to use death valley data
-
-
 import csv
 import datetime as dt  # MAGIC!
 
-open_file = open("sitka_weather_2018_simple.csv", "r")
+#!) Handle error checking using try and except
+# 2) change file to use death valley data
+
+open_file = open("death_valley_2018_simple.csv", "r")
 
 csv_file = csv.reader(open_file, delimiter=",")
 
 header_row = next(csv_file)
+# This skips the header row
+print(type(header_row))
+
 
 # Looking at index value as well as column header; what index value is associte with what record
-
 # The Enumerate() function returns both index of each item adn the value of each item as you loop through a list
 
+"""
 for index, header in enumerate(header_row):
     print("Index:", index, "Column Header:", header)
+"""
 
-# output is  a tuple!
 
 highs = []
 dates = []
 lows = []
-
 
 # Version 2 (in class):
 somedate = "2010-02-18"
 converted_date = dt.datetime.strptime(somedate, "%Y-%m-%d")
 # This is an example of how the converted datetime works; use the dt.datetime to make datetime work on your weird computer
 
+"""
+#VERSION 1
 for row in csv_file:
-    highs.append(int(row[5]))
+    highs.append(int(row[4]))  # Adjust for death valley file
     converted_date = dt.datetime.strptime(row[2], "%Y-%m-%d")
     dates.append(converted_date)
-    lows.append(int(row[6]))
+    lows.append(int(row[5]))
+"""
+
+# Oh no! error with a literal in the csv file.
+# Try and except allows us to re-route the code if we have bad information in the file.
+# So here's what we do:
+
+# VERSION 2
+for row in csv_file:
+    try:
+        high = int(row[4])
+        low = int(row[5])
+        converted_date = dt.datetime.strptime(row[2], "%Y-%m-%d")
+
+    except ValueError:
+        print(
+            f"Missing data for {converted_date}"
+        )  # "f" lets you write print statement w/out + and , and whatever
+
+    else:
+        highs.append(int(row[4]))
+        lows.append(int(row[5]))
+        dates.append(converted_date)
 
 
 # ctrl + option + n to run code
-
-
-# Let's extract the date to print on the graph with the highs
-# 1)
-# 2)
-# 3)
 
 
 import matplotlib.pyplot as plt
@@ -63,14 +82,5 @@ plt.tick_params(axis="both", labelsize=12)
 
 fig.autofmt_xdate()
 # Draws labels daigonnaly to prevent overlapping
-
-plt.show()
-
-##ASSIGNMENT!##
-
-fig2, a = plt.subplots(2)
-
-a[0].plot(dates, highs, c="red")
-a[1].plot(dates, lows, c="blue")
 
 plt.show()

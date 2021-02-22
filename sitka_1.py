758ecfc432de2c4cfa0faac1f6708bbb5b2c6b66
Abby Brown <abby_brown3@baylor.edu>
2021-02-22 13:38:42 -0600

    Working on video

--- sitka_1.py
+++ sitka_1.py
@@ -1,7 +1,30 @@
 import csv
 
-open_file = open("sitka_weather_07-2018_simple.csv", r)
+open_file = open("sitka_weather_07-2018_simple.csv", "r")
 
 csv_file = csv.reader(open_file, delimiter=",")
 
 header_row = next(csv_file)
+
+# Looking at index value as well as column header; what index value is associte with what record
+
+# The Enumerate() function returns both index of each item adn the value of each item as you loop through a list
+"""
+for index, header in enumerate(header_row):
+    print("Index:", index, "Column Header:", header)
+"""
+
+# it's a tuple!
+
+highs = []
+
+for row in csv_file:
+    highs.append(int(row[5]))
+
+# print(highs)
+
+import matplotlib.pyplot as plt
+
+plt.plot(highs, c="red")
+
+plt.show()
